870cc240f77877dd79972a2ba126d6bdb89a07f2
League Student <league@iMac-20.attlocal.net>
2019-12-26 16:59:20 -0800

    Finish Thread Pool

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,51 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	Robot blueRing;
+	Robot yellowRing;
+	Robot blackRing;
+	Robot greenRing;
+	Robot redRing;
+	public static void main(String[] args) {
+		OlympicRings_Threaded main = new OlympicRings_Threaded();
+		
+	}
+	public OlympicRings_Threaded() {
+		blueRing = new Robot(100,200);
+		yellowRing = new Robot(220,300);
+		blackRing = new Robot(340,200);
+		greenRing = new Robot(460,300);
+		redRing = new Robot(580,200);
+		
+		Thread r1 = new Thread(()->drawCircles(Color.blue, blueRing));
+		Thread r2 = new Thread(()->drawCircles(Color.yellow, yellowRing));
+		Thread r3 = new Thread(()->drawCircles(Color.black, blackRing));
+		Thread r4 = new Thread(()->drawCircles(Color.green, greenRing));
+		Thread r5 = new Thread(()->drawCircles(Color.red, redRing));
 
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+	}
+	
+	void drawCircles(Color color, Robot r) {
+		r.setPenWidth(15);
+		r.setSpeed(20);
+		r.penDown();
+		r.setPenColor(color);
+		for(int i = 0; i < 360; i++) {
+			r.move(2);
+			r.turn(1);
+		}
+		r.penUp();
+		r.hide();
+	}
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,107 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
+	int temp = 30;
+	Random rand = new Random();
+	ArrayList<Robot> racers = new ArrayList<Robot>();
+	Thread r1 = new Thread(() -> race(racers.get(0)));
+	Thread r2 = new Thread(() -> race(racers.get(1)));
+	Thread r3 = new Thread(() -> race(racers.get(2)));
+	Thread r4 = new Thread(() -> race(racers.get(3)));
+	Thread r5 = new Thread(() -> race(racers.get(4)));
+
+	// Re-do the robot race recipe from level 3 module 0.
 	// This time, use threads to make all of the robots go at the same time.
+	/** i never did this version of module 3 ^ :( **/
+	public static void main(String[] args) {
+		AdvancedRobotRace main = new AdvancedRobotRace();
+	}
+
+	public AdvancedRobotRace() {
+		for (int i = 0; i < 5; i++) {
+			racers.add(new Robot(temp, 600));
+			temp += 70;
+		}
+		for (Robot robot : racers) {
+			robot.setSpeed(10);
+			// robot.turn(180);
+		}
+		// Thread r1 = new Thread(() -> race(racers.get(0)));
+		// Thread r2 = new Thread(() -> race(racers.get(1)));
+		// Thread r3 = new Thread(() -> race(racers.get(2)));
+		// Thread r4 = new Thread(() -> race(racers.get(3)));
+		// Thread r5 = new Thread(() -> race(racers.get(4)));
+
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+
+		/*
+		 * if (racers.get(0).getY() <= 0) { JOptionPane.showMessageDialog(null,
+		 * "Robot 1 is the winner!"); } else if (racers.get(1).getY() <= 0) {
+		 * JOptionPane.showMessageDialog(null, "Robot 2 is the winner!"); } else if
+		 * (racers.get(2).getY() <= 0) { JOptionPane.showMessageDialog(null,
+		 * "Robot 3 is the winner!"); } else if (racers.get(3).getY() <= 0) {
+		 * JOptionPane.showMessageDialog(null, "Robot 4 is the winner!"); } else if
+		 * (racers.get(4).getY() <= 0) { JOptionPane.showMessageDialog(null,
+		 * "Robot 5 is the winner!"); } else { JOptionPane.showMessageDialog(null,
+		 * "Huh. No one finishes!"); }
+		 */
+		while (racers.get(0).getY() > 0 && racers.get(1).getY() > 0 && racers.get(2).getY() > 0
+				&& racers.get(3).getY() > 0 && racers.get(4).getY() > 0) {
+			System.out.println("nope");
+		}
+		
+		if (racers.get(0).getY() <= 0 && racers.get(1).getY() > 0 && racers.get(2).getY() > 0
+				&& racers.get(3).getY() > 0 && racers.get(4).getY() > 0) {
+			JOptionPane.showMessageDialog(null, "Robot 1 is the winner!");
+		} else if (racers.get(1).getY() <= 0 && racers.get(0).getY() > 0 && racers.get(2).getY() > 0
+				&& racers.get(3).getY() > 0 && racers.get(4).getY() > 0) {
+			JOptionPane.showMessageDialog(null, "Robot 2 is the winner!");
+		} else if (racers.get(2).getY() <= 0 && racers.get(0).getY() > 0 && racers.get(1).getY() > 0
+				&& racers.get(3).getY() > 0 && racers.get(4).getY() > 0) {
+			JOptionPane.showMessageDialog(null, "Robot 3 is the winner!");
+		} else if (racers.get(3).getY() <= 0 && racers.get(0).getY() > 0 && racers.get(1).getY() > 0
+				&& racers.get(2).getY() > 0 && racers.get(4).getY() > 0) {
+			JOptionPane.showMessageDialog(null, "Robot 4 is the winner!");
+		} else if (racers.get(4).getY() <= 0 && racers.get(0).getY() > 0 && racers.get(1).getY() > 0
+				&& racers.get(2).getY() > 0 && racers.get(3).getY() > 0) {
+			JOptionPane.showMessageDialog(null, "Robot 5 is the winner!");
+		} else if (racers.get(0).getY() > 0 && racers.get(1).getY() > 0 && racers.get(2).getY() > 0
+				&& racers.get(3).getY() > 0 && racers.get(4).getY() > 0) {
+			JOptionPane.showMessageDialog(null, "Huh. No one finishes!");
+		} else {
+			JOptionPane.showMessageDialog(null, "Huh. It was a tie!");
+		}
+
+	}
+
+	void race(Robot robadob) {
+
+		while (robadob.getY() > 0) {
+			if (racers.get(0).getY() > 0 && racers.get(1).getY() > 0 && racers.get(2).getY() > 0
+					&& racers.get(3).getY() > 0 && racers.get(4).getY() > 0) {
+				robadob.move(rand.nextInt(25));
+			} else if (racers.get(0).getY() <= 0 || racers.get(1).getY() <= 0 || racers.get(2).getY() <= 0
+					|| racers.get(3).getY() <= 0 || racers.get(4).getY() <= 0) {
+				// robadob.setY(100);
+			}
+		}
+
+		if (robadob.getY() <= 0) {
+			System.out.println("Robot has finished!");
+			// robadob.setY(-10);
+		}
+
+	}
+
 }

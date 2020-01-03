aa5379241ddf97b816ab94b8983128dcecfc271d
League Student <league@iMac-20.attlocal.net>
2020-01-02 16:57:22 -0800

    ThreadPool almost done? Worker, Threadpool, Task done by UML - something's up with the JUnit?

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,31 @@
 package _04_Thread_Pool;
 
-public class ThreadPool {
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class ThreadPool {
+	ConcurrentLinkedQueue<Task> taskQueue;
+	Thread[] threads;
+	public ThreadPool(int totalThreads) {
+		threads = new Thread[totalThreads];
+		// TODO Auto-generated constructor stub
+		for(int i = 0; i < totalThreads; i++) {
+			threads[i] = new Thread(()-> {Worker worker = new Worker(taskQueue);});
+		}
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+	}
+	public void addTask(Task Task) {
+		taskQueue.add(Task);
+	}
+
+	public void start() {
+		for(Thread t : threads) {
+			t.run();
+		}
+		try {
+			threads[0].join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,13 +1,18 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
-
+	ConcurrentLinkedQueue<Task> taskQueue;
+	Worker(ConcurrentLinkedQueue<Task> taskQueueIn) {
+		taskQueue = taskQueueIn;
+	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		while(!taskQueue.isEmpty()) {
+			taskQueue.remove().perform();
+		}
 	}
-
 
 }
